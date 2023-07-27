011e2b976b8747463d34ebff66131c6e7cdfbfb0
FERHATSUMER <sumerf39@gmail.com>
2023-07-28 00:25:53 +0300

    fullpage2

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,40 +1,44 @@
-import React from "react";
-import { PiBooks,PiExamLight } from "react-icons/pi";
-import { FiSettings } from "react-icons/fi";
-import { MdOutlineNotificationsActive } from "react-icons/md";
-import "./header.css";
+import React from 'react';
+import { PiBooks, PiExamLight } from 'react-icons/pi';
+import { FiSettings } from 'react-icons/fi';
+import { MdOutlineNotificationsActive } from 'react-icons/md';
+import './header.css';
 
 const Header = () => {
   return (
-    <header className="header-container">
-      <img
-        className="logo"
-        src="https://media.licdn.com/dms/image/D4D03AQFMfnp5H1M-vA/profile-displayphoto-shrink_800_800/0/1667210919849?e=2147483647&v=beta&t=SL-bp4f4k3ElkP3ot_ahz2KSGT-uH9OeKbtvYwBTajw"
-        alt="logo"
-      />
-      <nav className="nav">
+    <header className='header-container'>
+      <div className='header-right'>
+        <img
+          className='logo'
+          src='https://media.licdn.com/dms/image/C560BAQG50mwENt9gSQ/company-logo_400_400/0/1632727910976?e=2147483647&v=beta&t=0w36fnGQ37PWTm253SLL0PVTr2ziC_mzcyO71DlLIDc'
+          alt='logo'
+        />
+        <h1> Ferhat Sümer</h1>
+      </div>
+
+      <nav className='nav'>
         <ul>
           <li>
             <PiBooks />
-            <a href="https://www.linkedin.com/in/ferhatsumer/" target="_blank">
+            <a href='https://www.linkedin.com/in/ferhatsumer/' target='_blank'>
               <h4>Dersler</h4>
             </a>
           </li>
           <li>
             <PiExamLight />
-            <a href="https://github.com/FERHATSUMER" target="_blank">
+            <a href='https://github.com/FERHATSUMER' target='_blank'>
               <h4>Sınavlar</h4>
             </a>
           </li>
           <li>
             <MdOutlineNotificationsActive />
-            <a href="https://www.instagram.com/ferhatsumer_/" target="_blank">
+            <a href='https://www.instagram.com/ferhatsumer_/' target='_blank'>
               <h4>Bildirim</h4>
             </a>
           </li>
           <li>
             <FiSettings />
-            <a href="https://www.instagram.com/ferhatsumer_/" target="_blank">
+            <a href='https://www.instagram.com/ferhatsumer_/' target='_blank'>
               <h4>Ayarlar</h4>
             </a>
           </li>
--- src/components/assets/data.js
+++ src/components/assets/data.js
@@ -1,49 +1,49 @@
 const informations = [
     {
       id: 1,
-      img:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTZgN3mM3stlhsRJQGy6C6DevzxAgX1u6QBgg&usqp=CAU",
-      name: "Ferhat Sümer",
-      job:"Frontend-Developer",
+      img:"https://letgenbio.com/wp-content/uploads/2021/06/tuik-ilk-kez-acikladi-turkiyede-biyoteknoloji4b-1024x682.jpg",
+      name: "Biyoloji",
+      job:"Öğretmen Adı",
       about:
         "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Amet hic cumque iure eaque culpa accusamus magnam autem incidunt aut aspernatur! Nihil eius recusandae voluptas nam fugiat similique facilis fuga et!",
     },
     {
       id: 2,
-      img:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ1I02Xs2rnBYvBRiT9VKGzjLXlA1hMvJSL3w&usqp=CAU",
-      name: "Serkan Demir",
-      job:"Lavyer",
+      img:"https://media.istockphoto.com/id/1219382595/tr/vekt%C3%B6r/bir-tahtaya-yaz%C4%B1lan-matematik-denklemleri.jpg?s=612x612&w=0&k=20&c=KdDQ4aB9JdCGG-OBBB-9ESv3XuvcF5ZPiIgjCGA6YIc=",
+      name: "Matematik",
+      job:"Öğretmen Adı",
       about:
         "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Amet hic cumque iure eaque culpa accusamus magnam autem incidunt aut aspernatur! Nihil eius recusandae voluptas nam fugiat similique facilis fuga et!",
     },
     {
       id: 3,
-      name: "Erkan Güneş",
-      img:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSPevo9fuGWQOR7XYgCQHjrx-8_pdcTiWJDbQ&usqp=CAU",
-      job:"Turkish Airlines pilot",
+      name: "Tarih",
+      img:"https://www.tech-worm.com/wp-content/uploads/2019/03/fatih-sultan-mehmet-fatih-unvanini-nasil-ve-ne-zaman-almistir.jpg",
+      job:"Öğretmen Adı",
       about:
         "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Amet hic cumque iure eaque culpa accusamus magnam autem incidunt aut aspernatur! Nihil eius recusandae voluptas nam fugiat similique facilis fuga et!",
     },
     {
       id: 4,
-      name: "Yusuf Gümüş",
-      img:"https://muhendistan.com/wp-content/uploads/2019/05/M%C3%BChendis.M%C3%BChendistan-1.jpg",
-      job:"Engineer",
+      name: "Edebiyat",
+      img:"https://www.cumhuriyet.com.tr/Archive/2021/2/13/1813381/kapak_002828.jpg",
+      job:"Öğretmen Adı",
       about:
         "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Amet hic cumque iure eaque culpa accusamus magnam autem incidunt aut aspernatur! Nihil eius recusandae voluptas nam fugiat similique facilis fuga et!",
     },
     {
       id: 5,
-      name: "Burak Solmaz",
-      img:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTUV8lbFY52c4wHulrXwLbw5YuJ0rMhm9T0oA&usqp=CAU",
-      job:"Student",
+      name: "Coğrafya",
+      img:"https://www.arttablo.com/upload/U-dunya-haritasitasi-ve-pusula-2-hd-goruntu-eski-cizim-dunya-haritasi-cografya-canvas-tablo1474464986-800.jpg",
+      job:"Öğretmen Adı",
       about:
         "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Amet hic cumque iure eaque culpa accusamus magnam autem incidunt aut aspernatur! Nihil eius recusandae voluptas nam fugiat similique facilis fuga et!",
     },
     {
       id: 6,
-      name: "Arda Güler",
-      img:"https://www.islamveihsan.com/wp-content/uploads/2022/06/ogretmen-ne-demek-187882.jpg",
-      job:"Teacher",
+      name: "Fizik",
+      img:"https://www.bilimseldunya.com/wp-content/uploads/2022/03/Fizik-Nedir.jpg",
+      job:"Öğretmen Adı",
       about:
         "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Amet hic cumque iure eaque culpa accusamus magnam autem incidunt aut aspernatur! Nihil eius recusandae voluptas nam fugiat similique facilis fuga et!",
     },
